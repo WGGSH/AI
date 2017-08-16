1c2656ee18f7c4659eef392f2ae57b31de44622d
wgg <is0332ks@ed.ritsumei.ac.jp>
2017-08-16 12:17:18 +0900

    初期ファイル

---
+++ Hotel.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ateam
+{
+    public class Hotel : BaseBattleAISystem
+    {
+        //---------------------------------------------------
+        // InitializeAI
+        //---------------------------------------------------
+        override public void InitializeAI()
+        {
+
+        }
+
+        //---------------------------------------------------
+        // UpdateAI
+        //---------------------------------------------------
+        override public void UpdateAI()
+        {
+            
+        }
+
+        //---------------------------------------------------
+        // ItemSpawnCallback
+        //---------------------------------------------------
+        override public void ItemSpawnCallback(ItemSpawnData itemData)
+        {
+
+        } 
+    }
+}
